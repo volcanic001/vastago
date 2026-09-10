7740bcfc6b8197b3bfe16ad00746a8197500e65d
volcanic001 <davidfeliphe97@gmail.com>
2026-09-09 21:16:46 -0600

    chore: release v0.1.0

--- internal/cli/cli.go
+++ internal/cli/cli.go
@@ -15,7 +15,7 @@ import (
 	"github.com/volcanic001/vastago/internal/tui"
 )
 
-const version = "0.1.0-dev"
+const version = "0.1.0"
 
 func Run(args []string, stdout, stderr io.Writer) int {
 	dataPath, remaining, err := globalArgs(args)
